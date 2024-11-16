36e38831a33b8f754472ba8a9ca2d2ad417aef4b
rakRandom <fellipeleo100@gmail.com>
2024-11-16 11:29:55 -0300

    chore: update tailwind configs
    minified the output css and added some theme extensions, also added more paths to the content search

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./app/**/*.{html,js,php}"],
+    content: [
+        "./app/view/**/*.{html,js,php}",
+
+          // Checking for class reference in JS/TS files that could be in these folders
+        "./resources/**/*.{html,js,php}",
+        "./assets/**/*.{html,js,php}"
+    ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                white: "#fdfdfd",
+                black: "#202020",
+                primary: {
+                    DEFAULT: "#003366",
+                    dark: "#002952"
+                },
+                secondary: {
+                    DEFAULT: "#fed233",
+                    dark: "#000000"  // Undefined for now
+                }
+            }
+        },
     },
     plugins: []
 }

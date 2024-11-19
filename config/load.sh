1933ac2153b8e24fbc5115d709cba51526e4506f
rakRandom <fellipeleo100@gmail.com>
2024-11-19 18:41:57 -0300

    chore: update linux load script

--- config/load.sh
+++ config/load.sh
@@ -4,5 +4,12 @@ if [ -d "$directory" ]; then
     echo "Deleting: '$directory'"
     rm -r "/opt/lampp/htdocs/quickmart"
 fi
-echo "Copying: '.' to '$directory'"
-cp -R . $directory
+
+echo "Copying: 'app', 'assets', 'config' and 'public' to '$directory'"
+
+mkdir $directory
+
+cp -R ./app $directory/app
+cp -R ./assets $directory/assets
+cp -R ./config $directory/config
+cp -R ./public $directory/public

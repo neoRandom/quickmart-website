44a427377ed5865922e741dd12ced2998886f568
rakRandom <fellipeleo100@gmail.com>
2024-11-19 19:20:32 -0300

    fix: user page

--- app/view/user_page.php
+++ app/view/user_page.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> QuickMart - Admin Panel </title>
-    <link rel="stylesheet" href="../../assets/css/main.css">
+    <title> QuickMart | User Page </title>
+    <link rel="stylesheet" href="/quickmart/assets/css/main.css">
 </head>
 <body>
-    <div "flex flex-col gap-4 mt-16">
-        <h1 "font-bold text-4xl text-center">
+    <div class="flex flex-col gap-4 mt-16">
+        <h1 class="font-bold text-4xl text-center">
             Under Construction 🚧
         </h1>
-        <p "text-center">
+        <p class="text-center">
             This page isn't available yet ;)
         </p>
     </div>

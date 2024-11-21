03c6e2f66a02eece5f81cb47f727123b6b753142
rakRandom <fellipeleo100@gmail.com>
2024-11-21 01:05:47 -0300

    chore: add typescript to the project

--- resources/ts/Carousel.ts
+++ resources/ts/Carousel.ts
@@ -1,45 +1,60 @@
-"use strict";
 class Carousel {
-    constructor(container, ltr = true) {
+    container: HTMLElement;
+    positionsList: Array<string>;
+    currentPosition: number;
+    cycleFunctionID: number | null;
+    timeBetween: number;
+    leftToRight: boolean;
+
+    constructor(container: HTMLElement, ltr = true) {
         this.container = container;
         this.positionsList = ["0"];
         this.currentPosition = 0;
         this.cycleFunctionID = null;
         this.timeBetween = 6000;
         this.leftToRight = ltr;
+
         for (let i = 1; i < container.children.length; i++) {
             this.positionsList.push(`-${100 * i}%`);
         }
     }
+
     update() {
         this.container.style.transform = `translateX(${this.positionsList[this.currentPosition]})`;
     }
+
     next() {
         if (this.currentPosition >= this.positionsList.length - 1) {
             this.currentPosition = 0;
-        }
-        else {
+        } else {
             this.currentPosition++;
         }
         this.update();
     }
+
     previous() {
         if (this.currentPosition <= 0) {
             this.currentPosition = this.positionsList.length - 1;
-        }
-        else {
+        } else {
             this.currentPosition--;
         }
         this.update();
     }
+
     cycle() {
         if (this.cycleFunctionID === null)
             return;
-        this.cycleFunctionID = setInterval((this.leftToRight) ? () => { this.next(); } : () => { this.previous(); }, this.timeBetween);
+
+        this.cycleFunctionID = setInterval(
+            (this.leftToRight) ? () => {this.next()} : () => {this.previous()}, 
+            this.timeBetween
+        );
     }
+
     stop() {
         if (this.cycleFunctionID === null)
             return;
+
         clearInterval(this.cycleFunctionID);
         this.cycleFunctionID = null;
     }

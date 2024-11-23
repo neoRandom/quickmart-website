3bf8a46cad298dd4fb1f92046d16745b6026dc03
neoRandom <fellipeleo100@gmail.com>
2024-11-23 14:19:12 -0300

    chore: update scripts
    - update windows load script
    
    - update autoloader
    
    - add typescript to dev dependencies

--- app/autoload.php
+++ app/autoload.php
@@ -4,7 +4,7 @@ define("BASE_URL", "http://localhost/quickmart/public/");
 
 spl_autoload_register(
     function (string $className) {
-        $filePath = __DIR__ . '/' . str_replace('\\', '/', $className) . '.php';
+        $filePath = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
         error_log("Attempting to autoload: $className from $filePath");
         if (file_exists($filePath)) {
             require_once $filePath;
--- config/load.bat
+++ config/load.bat
@@ -6,6 +6,10 @@ if exist "%target%" (
     rmdir /s /q "%target%"
 )
 
-robocopy . "%target%" /e /njh /njs /ndl /nc /ns /xd .git node_modules resources docs database > nul
+mkdir "%target%"
+robocopy ./app "%target%/app" /e /njh /njs /ndl /nc /ns /xd > nul
+robocopy ./assets "%target%/assets" /e /njh /njs /ndl /nc /ns /xd > nul
+robocopy ./config "%target%/config" /e /njh /njs /ndl /nc /ns /xd > nul
+robocopy ./public "%target%/public" /e /njh /njs /ndl /nc /ns /xd > nul
 
 @echo on

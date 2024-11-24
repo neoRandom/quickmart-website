85a0f7bf645cdf1cc9eafbb26130479420d134f7
rakRandom <fellipeleo100@gmail.com>
2024-11-24 01:06:36 -0300

    feat: add admin ts utils base

---
+++ resources/ts/admin/utils.ts
@@ -0,0 +1,17 @@
+// Functions to show to the user something
+
+function showCreate() {}
+
+function showDetails() {}
+
+function showEdit() {}
+
+function showDelete() {}
+
+// Functions to post to the server
+
+function postCreate() {}
+
+function postEdit() {}
+
+function postDelete() {}
